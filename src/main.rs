b901f43ab1113f0b697432d0468f198ba70a548f
Jelte Dirks <j.dirks@outlook.com>
2023-03-20 21:44:20 +0100

    add the writing to stdout
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,9 @@ fn main() {
     write!(std::io::stdout(), "{settings}\n").unwrap();
     let mut result_list: Vec<std::path::PathBuf> = Vec::with_capacity((10 * settings.depth()) as usize);
     lsdep::walk_dirs(&settings, &mut result_list);
+
+    let mut stdo = std::io::stdout();
+    for a in result_list {
+        writeln!(stdo, "{}", a.display()).unwrap();
+    }
 }
